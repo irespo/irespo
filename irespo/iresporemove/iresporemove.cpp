e2cf05806fef5f292635ca98c97801142e0b38ea
Filip Nizioł <filip.niziol@gmail.com>
2018-07-03 20:31:58 +0200

    Adding one action to the contract

--- irespo/iresporemove/iresporemove.cpp
+++ irespo/iresporemove/iresporemove.cpp
@@ -4,4 +4,11 @@ using namespace eosio;
 class iresporemove : public eosio::contract {
 public:
 	using contract::contract;
+
+	/// @abi action 
+	void a(account_name user) {
+		
+	}
 };
+
+EOSIO_ABI(iresporemove, (a))
